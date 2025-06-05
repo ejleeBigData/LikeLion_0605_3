4dd81953ad44c1a8e12d4a33ef581a96f7accddf
이은정 <ej2grateful@gmail.com>
2025-06-05 13:47:56 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// 함수 연습
+public class Main {
+    public static void printStudent(String name, int age) {
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age);
+        System.out.println("-----------------");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---- 학생 정보 출력 ----");
+    }
+}
